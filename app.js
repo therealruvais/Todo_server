a8123115e80c623c9d2799dbf4bcf5a268aceded
therealruvais <ruvais666@gmail.com>
2024-04-24 09:40:01 +0530

    new

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-client-ruvais-projects.vercel.app",
     credentials: true,
   })
 );
